e749c9c84c70c01962daaf0592ee98261011d670
vijay <vijay@123.com>
2024-06-28 17:13:20 -0400

    index-form_V3

---
+++ webapp/src/main/webapp/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Details Form</title>
+</head>
+<body>
+    <h1>Enter Your Details</h1>
+    <form action="process_details.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br>
+
+        <label for="height">Height (in cm):</label>
+        <input type="number" id="height" name="height" required><br>
+
+        <label for="weight">Weight (in kg):</label>
+        <input type="number" id="weight" name="weight" required><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
